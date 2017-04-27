3a6db12829b55ff3c04a91b00167ed1a22ed15cb
hambone124 <hpe60m5@gmail.com>
2017-04-27 16:41:04 -0400

    TeamViewer User Access Token functions

--- TeamViewerPowerShell.psm1
+++ TeamViewerPowerShell.psm1
@@ -1,10 +1,4 @@
-﻿function Invoke-TeamViewerApiLogin {
-    param (
-        
-    )
-}
-
-function Get-TeamViewerApiAuthorizationCode {
+﻿function Get-TeamViewerApiAuthorizationCode {
     param (
         $ClientId,
         $RedirectUri
@@ -12,34 +6,81 @@ function Get-TeamViewerApiAuthorizationCode {
     $AuthorizationServer = "https://login.teamviewer.com/oauth2/authorize"
     $Uri = $AuthorizationServer + "?response_type=code&client_id=$ClientId&redirect_uri=$RedirectUri"
     start $Uri
-    $AuthorizationCode = Read-Host "Enter TeamViewer authorization code"
-    $AuthorizationCode
+    Read-Host "Enter TeamViewer authorization code"    
 }
 
-function Get-TeamViewerApiAccessToken {
+function Get-TeamViewerUserAccessToken {
     param (
-        [Parameter(Mandatory)]$AuthorizationCode
+        [Parameter(Mandatory)]$AuthorizationCode,
+        [Parameter(Mandatory)]$ClientId,
+        [Parameter(Mandatory)]$ClientSecret,
+        $RedirectUri
     )
     $Uri = "https://webapi.teamviewer.com/api/v1/oauth2/token"
-    $code = $AuthorizationCode
-    $ClientId = (Get-PasswordstateCredential -PasswordID 4127 -AsPlainText).UserName
-    $ClientSecret = (Get-PasswordstateCredential -PasswordID 4127 -AsPlainText).Password
-    $RedirectUri = "https://somenonexistentoauthclient.com/oauth"
-    $Body = "grant_type=authorization_code&code=$code&redirect_uri=$RedirectUri&client_id=$ClientId&client_secret=$ClientSecret"
+    $Body = "grant_type=authorization_code&code=$AuthorizationCode&redirect_uri=$RedirectUri&client_id=$ClientId&client_secret=$ClientSecret"
     Invoke-RestMethod -Method Post -Uri $Uri -ContentType application/x-www-form-urlencoded -Body $Body
 }
 
-function Export-TeamViewerApiTokenFile {
+function Update-TeamViewerUserAccessTokenFile {
+    param (
+        $TeamViewerAccessTokenFile = "~\TeamViewerUserAccessToken.xml",
+        [Parameter(Mandatory)]$ClientId,
+        [Parameter(Mandatory)]$ClientSecret
+    )
+    $EncryptedRefreshToken = (Import-Clixml -Path $TeamViewerAccessTokenFile).RefreshToken
+    $RefreshToken = Get-ValueFromEncryptedString -String $EncryptedRefreshToken
+    $Uri = "https://webapi.teamviewer.com/api/v1/oauth2/token"
+    $Body = "grant_type=refresh_token&refresh_token=$RefreshToken&client_id=$ClientId&client_secret=$ClientSecret"
+    $NewTeamViewerUserAccessToken = Invoke-RestMethod -Method Post -Uri $Uri -Body $Body -ContentType application/x-www-form-urlencoded
+    $NewTeamViewerUserAccessToken | Export-TeamViewerUserAccessTokenToFile -Path $TeamViewerAccessTokenFile
+}
+
+function Export-TeamViewerUserAccessTokenToFile {
     param (
         [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$access_token,
         [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$token_type,
         [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$expires_in,
-        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$refresh_token
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$refresh_token,
+        $Path = "~\TeamViewerUserAccessToken.xml"
     )
     process {
-        $AccessToken = 
+        $AccessToken = ConvertTo-SecureString -String $access_token -AsPlainText -Force | ConvertFrom-SecureString
+        [DateTime]$ExpirationDate = (Get-Date).AddMinutes($expires_in)
+        $RefreshToken = ConvertTo-SecureString -String $refresh_token -AsPlainText -Force | ConvertFrom-SecureString
         [PSCustomObject][Ordered]@{
-            AccessToken = $access_token
-        }
+            AccessToken = $AccessToken
+            TokenType = $token_type
+            ExpirationDate = $ExpirationDate
+            RefreshToken = $RefreshToken
+        } | Export-Clixml -Path $Path -Force
     }
+}
+
+function Import-TeamViewerUserAccessTokenFromFile {
+    param (
+        $Path = "~\TeamViewerUserAccessToken.xml"
+    )
+    try {
+        $UserAccessTokenObject = Import-Clixml -Path $Path
+    } catch {
+        throw "TeamViewer User Access Token file not found at $Path."
+    }
+    $CurrentDate = Get-Date
+    if ($UserAccessTokenObject.ExpirationDate -lt $CurrentDate) {
+        Update-TeamViewerUserAccessTokenFile -TeamViewerAccessTokenFile $Path
+        $UserAccessTokenObject = Import-Clixml -Path $Path
+    }
+    [PSCustomObject][Ordered]@{
+        UserAccessToken = Get-ValueFromEncryptedString -String $UserAccessTokenObject.AccessToken
+        TokenType = $UserAccessTokenObject.TokenType
+    }        
+}
+
+function Get-ValueFromEncryptedString {
+    param (
+        [Parameter(Mandatory)]$String
+    )
+    $SecureString = ConvertTo-SecureString -String $String 
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString)
+    [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 }
